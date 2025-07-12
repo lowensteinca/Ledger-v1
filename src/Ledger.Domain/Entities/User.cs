48f8f98127417e205ee324cd54dc30705937c4b0
Michael Lowenstein <michael@lowenstein.ca>
2025-07-12 13:25:45 -0600

    user entity updates.

---
+++ src/Ledger.Domain/Entities/User.cs
@@ -0,0 +1,33 @@
+// src/Ledger.Domain/Entities/User.cs
+using System.Diagnostics.Contracts;
+using LifeLedger.Domain.Events;
+using LifeLedger.Domain.Relations;
+
+namespace LifeLedger.Domain.Entities;
+
+public class User : BaseEntity<UserId>
+{
+    // private readonly List<LogBook> logBooks = new();
+
+    public string Username { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string FirstName { get; private set; } = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+
+    // public IReadOnlyCollection<LogBook> LogBooks => logBooks.AsReadOnly();
+
+    private User() { }
+
+    // public static User Create(string userrname, string email, string firstName, string lastName)
+
+    // public void UpdateProfile(string firstName, string lastName) { }
+
+    // public LogBook CreateLogBook(string title, string? description = null)
+    // {
+    //     var logBook = LogBook.Create(Id, title, description);
+    //     _logBooks.Add(logBook);
+    //     return logBook;
+    // }
+}
